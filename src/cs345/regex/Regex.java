01d814b74e641887f3e25e33c3c2da47023afedd
Terence Parr <parrt@antlr.org>
2016-01-06 10:49:13 -0800

    expand tabs

--- src/cs345/regex/Regex.java
+++ src/cs345/regex/Regex.java
@@ -11,64 +11,64 @@ package cs345.regex;
  *  https://swtch.com/~rsc/regexp/regexp1.html
  */
 public class Regex {
-	protected String pattern;
+    protected String pattern;
 
-	public Regex(String pattern) {
-		this.pattern = pattern;
-	}
+    public Regex(String pattern) {
+        this.pattern = pattern;
+    }
 
-	/** Parse the regex pattern and return an NFA that matches strings
-	 *  in the language described by the pattern.
-	 *
-	 *  See http://algs4.cs.princeton.edu/lectures/54RegularExpressions.pdf
-	 *      https://swtch.com/~rsc/regexp/regexp1.html
-	 *	    http://www.cs.may.ie/staff/jpower/Courses/Previous/parsing/node5.html
-	 *
-	 *  Here are the pattern constructs to NFA conversions (x,y are themselves
-	 *  regex patterns and o implies we construct a new state):
-	 *
-	 *  Empty string		o--->o
-	 *
-	 *  Letter a			o-a->o
-	 *
-	 *  xy					o->[x]->[y] 			(x.stop = y.start)
-	 *
-	 *  x | y				o->[x]->o
-	 * 						|		^
-	 * 					 	|->[y]--|
-	 *
-	 * 					    |------|
-	 *                      v 	   |
-	 * 	x*					o->[x]-|->o
-	 * 						|	      ^
-	 * 						|---------|
-	 */
-	protected NFA compile() {
-		RegexParser parser = new RegexParser(pattern);
-		return parser.parse();
-	}
+    /** Parse the regex pattern and return an NFA that matches strings
+     *  in the language described by the pattern.
+     *
+     *  See http://algs4.cs.princeton.edu/lectures/54RegularExpressions.pdf
+     *      https://swtch.com/~rsc/regexp/regexp1.html
+     *      http://www.cs.may.ie/staff/jpower/Courses/Previous/parsing/node5.html
+     *
+     *  Here are the pattern constructs to NFA conversions (x,y are themselves
+     *  regex patterns and o implies we construct a new state):
+     *
+     *  Empty string        o--->o
+     *
+     *  Letter a            o-a->o
+     *
+     *  xy                  o->[x]->[y]             (x.stop = y.start)
+     *
+     *  x | y               o->[x]->o
+     *                      |       ^
+     *                      |->[y]--|
+     *
+     *                      |------|
+     *                      v      |
+     *  x*                  o->[x]-|->o
+     *                      |         ^
+     *                      |---------|
+     */
+    protected NFA compile() {
+        RegexParser parser = new RegexParser(pattern);
+        return parser.parse();
+    }
 
-	public boolean matches(String input) {
-		NFA nfa = compile();
-		nfa.stop.accept(1); // mark final state as accept state for alt 1
-		int alt = simulate(nfa.start, input.toCharArray(), 0);
-		return alt==1;
-	}
+    public boolean matches(String input) {
+        NFA nfa = compile();
+        nfa.stop.accept(1); // mark final state as accept state for alt 1
+        int alt = simulate(nfa.start, input.toCharArray(), 0);
+        return alt==1;
+    }
 
-	public int simulate(NFA.State p, char[] input, int i) {
-		/* TODO:
-		if accept state:
-			if still have input left over, return NFA.FAIL
-			return p.accept
+    public int simulate(NFA.State p, char[] input, int i) {
+        /* TODO:
+        if accept state:
+            if still have input left over, return NFA.FAIL
+            return p.accept
 
-		for each edge e of p:
-			if epsilon edge:
-				simulate e
-				if that succeeds, return success
-			else if we have more input and e.label is current input symbol:
-				simulate e
-				if that succeeds, return success
-		return NFA.FAIL
-		*/
-	}
+        for each edge e of p:
+            if epsilon edge:
+                simulate e
+                if that succeeds, return success
+            else if we have more input and e.label is current input symbol:
+                simulate e
+                if that succeeds, return success
+        return NFA.FAIL
+        */
+    }
 }
