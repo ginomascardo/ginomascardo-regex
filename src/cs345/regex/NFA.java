d0cf55151df90db821f7c23a8403c87b0a4f7a57
Terence Parr <parrt@antlr.org>
2016-01-06 10:48:13 -0800

    add initial code

---
+++ src/cs345/regex/NFA.java
@@ -0,0 +1,111 @@
+package cs345.regex;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+/** An NFA is a collection of numbered states that have labeled, directed edges
+ *  between them. It's a Directed (possibly) Cyclic Graph.  An instance of
+ *  an NFA just tracks the outermost start and stop states of a graph.
+ *  Matching begins at the start state and finishes at the stop/accept state.
+ */
+public class NFA {
+	public static final int FAIL = 0; // invalid value for accept in stop state
+	public static final char EPSILON = 0;
+	protected static int globalStateCounter = 0;
+
+	public static class State {
+		public final int stateNumber; // state number
+		public final List<Edge> edges = new ArrayList<>();
+		protected int accept = FAIL;
+
+		public State() { stateNumber = globalStateCounter++; }
+		public void edge(char label, State target) {
+			Edge e = new Edge(label, target);
+			edges.add(e);
+		}
+		public void epsilon(State target) {
+			edge(EPSILON,target);
+		}
+		public void accept(int alt) { accept = alt; }
+		public String toString() { return "s"+stateNumber; }
+	}
+
+	public static class Edge {
+		State target;
+		char label;
+
+		public Edge(char label, State target) {
+			this.label = label;
+			this.target = target;
+		}
+	}
+
+	protected State start;
+	protected State stop;
+
+	public NFA() {
+		start = new State();
+		stop = new State();
+	}
+
+	public NFA(NFA old) {
+		this.start = old.start;
+		this.stop = old.stop;
+	}
+
+	public NFA(State start, State stop) {
+		this.stop = stop;
+		this.start = start;
+	}
+
+	public static NFA error() { return atom('\uFFFF'); }
+
+	public String toDOT() {
+		StringBuilder buf = new StringBuilder();
+		Set<Integer> visited = new LinkedHashSet<>();
+		buf.append("digraph nfa {\n");
+		buf.append("  rankdir = LR;\n");
+		buf.append("  node [shape = circle, height = 0.45, fontsize=18, fixedsize=true];\n");
+		toDOT_(start, buf, visited);
+		buf.append("}\n");
+		return buf.toString();
+	}
+
+	protected void toDOT_(State p, StringBuilder buf, Set<Integer> visited) {
+		/* TODO:
+		if we have visited p before, return
+		mark p as visited
+		for each edge e of p:
+			if epsilon edge, label is "&epsilon;"
+			else label is e.label
+			append "  %d -> %d [label=\"%s\", fontsize=18];\n" to buf
+				with appropriate p state number, target state number, and label
+			recurse to toDOT_() with appropriate parameters
+		*/
+	}
+
+	/** Return an ordered set of states obtained via a depth-first walk */
+	public LinkedHashSet<State> getStates() {
+		LinkedHashSet<State> visited = new LinkedHashSet<>();
+		getStates_(start, visited);
+		return visited;
+	}
+
+	protected void getStates_(State p, LinkedHashSet<State> visited) {
+        /* TODO:
+        if we have visited p before, return
+        mark p as visited
+        for each edge e of p:
+            recurse to getStates_() with appropriate parameters
+        */
+	}
+
+	public static NFA atom(char label) {
+		State a = new State();
+		State b = new State();
+		a.edge(label, b);
+		return new NFA(a,b);
+	}
+}
---
+++ src/cs345/regex/Regex.java
@@ -0,0 +1,74 @@
+package cs345.regex;
+
+/** Represents a regular expression of the following form (ANTLR notation):
+ *
+ *  regex    : sequence ('|' sequence)* ;
+ *  sequence : closure sequence | ; // same as closure* but easier to build NFA
+ *  closure  : element '*' | element ;
+ *  element  : letter | '(' regex ')' ;
+ *
+ *  To learn all about regex, see Russ Cox's amazing document:
+ *  https://swtch.com/~rsc/regexp/regexp1.html
+ */
+public class Regex {
+	protected String pattern;
+
+	public Regex(String pattern) {
+		this.pattern = pattern;
+	}
+
+	/** Parse the regex pattern and return an NFA that matches strings
+	 *  in the language described by the pattern.
+	 *
+	 *  See http://algs4.cs.princeton.edu/lectures/54RegularExpressions.pdf
+	 *      https://swtch.com/~rsc/regexp/regexp1.html
+	 *	    http://www.cs.may.ie/staff/jpower/Courses/Previous/parsing/node5.html
+	 *
+	 *  Here are the pattern constructs to NFA conversions (x,y are themselves
+	 *  regex patterns and o implies we construct a new state):
+	 *
+	 *  Empty string		o--->o
+	 *
+	 *  Letter a			o-a->o
+	 *
+	 *  xy					o->[x]->[y] 			(x.stop = y.start)
+	 *
+	 *  x | y				o->[x]->o
+	 * 						|		^
+	 * 					 	|->[y]--|
+	 *
+	 * 					    |------|
+	 *                      v 	   |
+	 * 	x*					o->[x]-|->o
+	 * 						|	      ^
+	 * 						|---------|
+	 */
+	protected NFA compile() {
+		RegexParser parser = new RegexParser(pattern);
+		return parser.parse();
+	}
+
+	public boolean matches(String input) {
+		NFA nfa = compile();
+		nfa.stop.accept(1); // mark final state as accept state for alt 1
+		int alt = simulate(nfa.start, input.toCharArray(), 0);
+		return alt==1;
+	}
+
+	public int simulate(NFA.State p, char[] input, int i) {
+		/* TODO:
+		if accept state:
+			if still have input left over, return NFA.FAIL
+			return p.accept
+
+		for each edge e of p:
+			if epsilon edge:
+				simulate e
+				if that succeeds, return success
+			else if we have more input and e.label is current input symbol:
+				simulate e
+				if that succeeds, return success
+		return NFA.FAIL
+		*/
+	}
+}
